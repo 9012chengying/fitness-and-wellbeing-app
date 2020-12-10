12539fdb1f7f708918d2eefb70a2a48b34bb3d35
Angela Boakes <boakesa3@cardiff.ac.uk>
2020-12-10 17:24:12 +0000

    added route mapping in controller

--- src/main/java/uk/ac/cf/nsa/web/phyt/controllers/GeneralControllers.java
+++ src/main/java/uk/ac/cf/nsa/web/phyt/controllers/GeneralControllers.java
@@ -1,9 +1,15 @@
 package uk.ac.cf.nsa.web.phyt.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GeneralControllers {
+
+@RequestMapping(path="/trainer/clients")
+public String trainerAllClients(String name) {
+    return "clients";
+}
 
 
 }
