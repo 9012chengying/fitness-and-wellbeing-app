faf9178e9d3bf8c7163f4d4533ec6114128dbc71
Angela Boakes <boakesa3@cardiff.ac.uk>
2020-12-10 12:04:12 +0000

    initialise springboot project

---
+++ src/main/java/uk/ac/cf/nsa/web/phyt/PhytApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.cf.nsa.web.phyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhytApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhytApplication.class, args);
+    }
+
+}
