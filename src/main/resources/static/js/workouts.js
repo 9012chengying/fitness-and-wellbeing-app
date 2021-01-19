4e59c8cd9152566da0e83bd8edc3e8e17ad8656a
Rebecca Rix <rixr@cardiff.ac.uk>
2021-01-19 14:06:23 +0000

    Added basic timer to timer page

--- src/main/resources/static/js/workouts.js
+++ src/main/resources/static/js/workouts.js
@@ -1,3 +1,5 @@
+//Exercise Description Window
+
 function openWin(exerciseLink) {
     window.open(exerciseLink, '_self');
 }
@@ -5,4 +7,24 @@ function openWin(exerciseLink) {
 function closeWin() {
     window.history.go(-1);
     return false;
+}
+
+//Start and stop timer
+let secondIncrease = setInterval(timer, 1000); //timeout is taken from workoutLength - th:data-id
+let sec = 0;
+function pad ( val ) {
+    return val > 9 ? val : "0" + val;
+}
+function timer() {
+    $("#seconds").html(pad(++sec%60));
+    $("#minutes").html(pad(parseInt(sec/60,10)));
+}
+function stopTimer() {
+    clearInterval(secondIncrease);
+}
+function workoutComplete(workoutLength) {
+    setTimeout(stopTimer, workoutLength);
+    //congratulations you've completed the workout message
+    //automatically updates database to show completed
+    //sends message to trainer
 }
