afc095100f523ddcfaa44da9220093873b5a049f
Rebecca Rix <rixr@cardiff.ac.uk>
2021-01-20 17:14:05 +0000

    Added array lists for timer in front-end

--- src/main/resources/static/js/workouts.js
+++ src/main/resources/static/js/workouts.js
@@ -11,26 +11,30 @@ function closeWin() {
 
 //Timer
 
-window.onload = function() {
-
+function timer() {
+    console.log("timer initiated");
     let interval;
     let rest = true;
     let seconds = 3;
     let exerciseIndex = 0;
-    let exerciseCount = exerciseCount;
+    let exerciseCount = exerciseCountData;
     let repIndex = 1;
-    let repTotal = reps;
-    let exerciseLength = exerciseLength;
-    let restLength = restLength;
-    let repRest = repRest;
+    let repTotal = repsData;
+    let exerciseLength = exerciseLengthData;
+    let restLength = restLengthData;
+    let repRest = repRestData;
     let startButton = document.getElementById("startButton");
     let stopButton = document.getElementById("stopButton");
     let resetButton = document.getElementById("resetButton");
     let instruction = document.getElementById("instruction");
+    let thumbnail = document.getElementById("thumbnail");
     let secondsSpan = document.getElementById("seconds");
-    let exerciseArrayList = ["exercise1", "exercise2", "exercise3", "exercise4"];
+    let exerciseArray = exerciseArrayData;
+    let thumbnailArray = thumbnailArrayData;
+    let workoutLength = workoutLengthData;
 
     startButton.onclick = function() {
+        console.log(exerciseCount + " is Exercise Count");
         rest = true;
         start();
         interval = setInterval(countdownSeconds, 1000);
@@ -39,29 +43,13 @@ window.onload = function() {
         $("#resetButton").css("display","inline-block");
     }
 
-    resetButton.onclick = function() {
-        clearInterval(interval);
-        rest = true;
-        start();
-        interval = setInterval(countdownSeconds, 1000);
-    }
-
-    stopButton.onclick = function() { //need to figure out how to get button to continue where it left off
-        clearInterval(interval);
-    }
-
-    function start() {
-        $("body").css("background","#fdfd96");
-        instruction.innerHTML = "Get Ready";
-    }
-
     function countdownSeconds() {
         seconds -= 1;
         secondsSpan.innerHTML = seconds;
         checkForStateChange();
     }
 
-    function checkForStateChange() {
+        function checkForStateChange() {
         if (exerciseIndex < exerciseCount) {
             if (seconds == 0 && rest == false) {
                 seconds = restLength + 1;
@@ -81,25 +69,44 @@ window.onload = function() {
             setTimeout(changeToRepRest, 1000);
         } else if (exerciseIndex == exerciseCount && repIndex == repTotal) {
             clearInterval(interval);
-            instruction.innerHTML = "Workout Complete" //didn't change
+            instruction.innerHTML = "Workout Complete" //check working - change to modal maybe
             secondsSpan.innerHTML = "Total Time was " + workoutLength;
             //automatically updates database to show completed
             //sends message to trainer
         }
     }
 
+    function start() {
+        $("body").css("background","#fdfd96");
+        instruction.innerHTML = "Get Ready";
+    }
+
     function changeToRest() {
         $("body").css("background","#63D1F4");
         instruction.innerHTML = "Rest";
+        thumbnail.src = "";
+    }
+
+    function changeToExercise() {
+        $("body").css("background", "pink");
+        instruction.innerHTML = exerciseArray[exerciseIndex]; //why is this underlined? works fine
+        thumbnail.src = thumbnailArray[exerciseIndex];
     }
 
     function changeToRepRest() {
         $("body").css("background","#fdfd96");
         instruction.innerHTML = "Long Rest";
+        thumbnail.src = "";
     }
 
-    function changeToExercise() {
-        $("body").css("background", "pink");
-        instruction.innerHTML = exerciseArrayList[exerciseIndex];
-        }
+    resetButton.onclick = function() {
+        clearInterval(interval);
+        rest = true;
+        start();
+        interval = setInterval(countdownSeconds, 1000);
+    }
+
+    stopButton.onclick = function() { //need to figure out how to get button to continue where it left off
+        clearInterval(interval);
+    }
 }
