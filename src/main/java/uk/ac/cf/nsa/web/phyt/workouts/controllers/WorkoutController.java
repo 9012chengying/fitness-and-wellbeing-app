5e6c1840128dcebaf77b400d6bdc31b60d6ac81b
Rebecca Rix <rixr@cardiff.ac.uk>
2021-01-21 19:05:52 +0000

    Added user service to workout controllers

--- src/main/java/uk/ac/cf/nsa/web/phyt/workouts/controllers/WorkoutController.java
+++ src/main/java/uk/ac/cf/nsa/web/phyt/workouts/controllers/WorkoutController.java
@@ -6,24 +6,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import uk.ac.cf.nsa.web.phyt.users.data.DTO.UserEntity;
+import uk.ac.cf.nsa.web.phyt.users.service.UserService;
 import uk.ac.cf.nsa.web.phyt.workouts.repository.WorkoutRepository;
 
 @Controller
 @RequestMapping(path="/client/workout")
 public class WorkoutController {
+
     private WorkoutRepository workoutRepository;
+    private UserService userService;
 
     @Autowired
-    public WorkoutController(WorkoutRepository pRepo) {
-        workoutRepository = pRepo;
+    public WorkoutController(WorkoutRepository workoutRepository, UserService userService) {
+        this.workoutRepository = workoutRepository;
+        this.userService = userService;
     }
 
     @GetMapping(path="")
-    public ModelAndView latestWorkout(/*@RequestParam(value="clientID", defaultValue="null") int clientID*/) { //client ID should come from login - this is just to get it to work for now
+    public ModelAndView latestWorkout() {
         ModelAndView mav = new ModelAndView();
-        int workoutID = workoutRepository.findIncompleteWorkoutID(2); //client ID should come from login - this is just to get it to work for now
+        UserEntity currentUser = userService.authenticateUser();
+        int userID = currentUser.getUserId();
+        int workoutID = workoutRepository.findIncompleteWorkoutID(userID);
         if (workoutID == -1) {
-            mav.setViewName("NoNewWorkouts"); //maybe change to restController or modal
+            mav.setViewName("NoNewWorkouts");
         } else {
             mav.addObject("workout", workoutRepository.workoutOverview(workoutID));
             mav.addObject("exercises", workoutRepository.workoutExerciseDetails(workoutID));
